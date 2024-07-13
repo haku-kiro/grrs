a8a533b1d4cd8a8782dbc1094f191e6ce4d67125
mdjcosta <botPrime@yahoo.com>
2024-07-13 13:57:55 +0200

    Instead using an external library, clap
    We can use clap to parse our arguments, and it comes with the benefit of parsing any future arguments that we add - but also displaying a useful help message that shows proper usage, and required arguments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no file path given");
-
-    // One way to handle the data conversion
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
