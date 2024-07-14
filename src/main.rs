08bfc42b8174bcb7896c09d4c0d491b335780f1a
mdjcosta <botPrime@yahoo.com>
2024-07-14 15:22:34 +0200

    Working solution with context

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file `{:?}`", &args.path))?;
-    println!("{}", content);
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
     Ok(())
 }
