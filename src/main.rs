b59d4acab818fdf1a925065c1ec7cced35c3e422
mdjcosta <botPrime@yahoo.com>
2024-07-14 14:49:55 +0200

    Handling result unrwap with a question mark operator

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => { content },
-        Err(err) => { return Err(err.into()); },
-    };
-
+    let content = std::fs::read_to_string(&args.path)?;
     println!("{}", content);
     Ok(())
 }
