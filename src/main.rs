b39ca5f1d22ea5f7209df34e8bc26aba90aee429
mdjcosta30 <mdjcosta30@gmail.com>
2024-07-15 20:16:45 +0200

    Added unit test
    Showed how you would split out logic, so that it is more easily tested. Added a unit test to do so.

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,30 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
+
+// Note the third parameter, it's a trait - allows for our function to be generic, i.e.
+// testable
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            // Not sure about the expect usage here
+            writeln!(writer, "{}", line).expect("Writing has failed");
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file `{:?}`", &args.path))?;
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[test]
-fn check_answer_validty() {
-    assert_eq!(answer(), 42);
-}
-
-fn answer() -> usize {
-    42
 }
